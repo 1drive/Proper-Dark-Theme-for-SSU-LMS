7c99eb4b68a00f934120794cd1d90196c9030bcb
Seungheon Han <appcloud@naver.com>
2021-09-06 22:33:09 +0900

    first commit

---
+++ content.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function (e) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '#';
+    document.head.appendChild(link);
+}, false);
