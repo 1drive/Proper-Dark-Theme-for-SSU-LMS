13a29178a7ad160904e700f74a58f3fee95a8da7
Seungheon Han <appcloud@naver.com>
2021-09-19 21:03:06 +0900

    CSS 수정 및 external_tools 유지하는 기능 추가(Beta)
    과제, 파일 등 CSS 요소가 적용되지 않은 곳들에 대해 수정을 하였고, "과목"에서 과목 이동시 external_tools/*이 유지됩니다.

--- content.js
+++ content.js
@@ -4,4 +4,28 @@ window.addEventListener('load', function (e) {
     link.type = 'text/css';
     link.href = '#';
     document.head.appendChild(link);
+    getURLforMENU();
 }, false);
+
+var navButton = document.getElementById('global_nav_courses_link');
+if (navButton) {navButton.addEventListener('click', function(event){
+    event.preventDefault();
+    getURLforMENU();
+  });}
+
+function getURLforMENU() {
+    var elements = document.getElementsByClassName('fOyUs_bGBk fbyHH_bGBk fbyHH_bSMN');
+    if (elements.length > 0) {
+        var currentURL = window.location.href;
+        var last17digits = currentURL.substr(currentURL.length-17, 17);
+        console.log(last17digits.substr(0, 4));
+    for (var i = 0, len = elements.length; i < len; i++) {
+        var currentURL = window.location.href;
+        var last17digits = currentURL.substr(currentURL.length-17, 17);
+        if (last17digits.substr(0, 4) == "/ext") {
+        elements[i].href = elements[i].href + last17digits;
+        }
+    }}
+    else{
+    }
+}
